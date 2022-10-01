f8d4f0756b29d36c0dcd1ef81227749cd1f55d4d
Cheryl M <webmaster@cherylli.com>
2022-10-01 11:07:50 +1000

    add swagger api docs

--- server/index.js
+++ server/index.js
@@ -9,13 +9,22 @@ const linksRouter = require('./src/routes/links.route');
 const app = express();
 app.use(express.json());
 
+// setup swagger
+const YAML = require('yamljs')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocument = YAML.load('./src/docs.yaml')
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 // middlewares
 
 // routes
 app.use('/api/links', linksRouter);
 
-app.get('/', (req, res) => {
-    res.send(`Welcome to &lt;LinkShortener&gt; API`);
+app.get('/', (req,res) =>{
+    res.send('Express backend running')
+})
+app.get('/api', (req, res) => {
+    res.send({message: `Welcome to &lt;LinkShortener&gt; API`});
 });
 
 // mongodb and express server connection
