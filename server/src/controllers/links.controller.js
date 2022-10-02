6929a41d8048ad8544ba62c2c0d54be8e34ab1a8
harshil sharma <harshilsharmaa0@gmail.com>
2022-10-02 19:54:10 +0530

    Edit URL (Api Added)

--- server/src/controllers/links.controller.js
+++ server/src/controllers/links.controller.js
@@ -1,12 +1,12 @@
-const {getRandomString} = require('../utils/getRandomString')
+const { getRandomString } = require('../utils/getRandomString')
 const Link = require('../models/link')
 
 getAllLinks = async (req, res) => {
     const allLinks = await Link.find({})
-    if(allLinks){
+    if (allLinks) {
         return res.status(200).json({
             "success": true,
-            "data" : allLinks
+            "data": allLinks
         })
     }
     res.status(500).json({
@@ -16,14 +16,14 @@ getAllLinks = async (req, res) => {
 }
 
 createLink = async (req, res) => {
-    if(!req.body.url){
-        return res.status(500).json({"success": false, "message":"URL is required."})
+    if (!req.body.url) {
+        return res.status(500).json({ "success": false, "message": "URL is required." })
     }
-    if (!/(www|http:|https:)+[^\s]+[\w]/g.test(req.body.url)){
+    if (!/(www|http:|https:)+[^\s]+[\w]/g.test(req.body.url)) {
         return res.status(500).json({ "success": false, "message": "invalid url" })
     }
     const randomString = await getRandomString()
-    try{
+    try {
         const link = new Link({
             url: req.body.url,
             short: randomString
@@ -33,12 +33,38 @@ createLink = async (req, res) => {
             "original_url": savedLink.url,
             "short_url": savedLink.short
         })
-    }catch (error){
-        res.status(500).json({"success": false, "message":error.message})
+    } catch (error) {
+        res.status(500).json({ "success": false, "message": error.message })
+    }
+}
+
+editLink = async (req, res) => {
+    try {
+        if(!req.body.url){
+            return res.status(500).json({ "success": false, "message": "URL is required." })
+        }
+
+        const link = await Link.findById(req.params.urlID)
+        if(!link){
+            return res.status(500).json({ "success": false, "message": "URL not found." })
+        }
+        
+        if (!/(www|http:|https:)+[^\s]+[\w]/g.test(req.body.url)) {
+            return res.status(500).json({ "success": false, "message": "invalid url" })
+        }
+
+        link.url = req.body.url
+        await link.save();
+
+        res.status(200).json({"success": true, "message": "URL updated successfully."});
+    } 
+    catch (error) {
+        res.status(500).json({ "success": false, "message": error.message })
     }
 }
 
 module.exports = {
     getAllLinks,
-    createLink
+    createLink,
+    editLink
 }
--- server/src/routes/links.route.js
+++ server/src/routes/links.route.js
@@ -3,6 +3,7 @@ const linksController = require('../controllers/links.controller')
 
 router.get('/', linksController.getAllLinks)
 router.post('/', linksController.createLink)
+router.put('/:urlID', linksController.editLink)
 
 
 module.exports = router
