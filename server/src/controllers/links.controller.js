d3ef057621941c9fdc118c5d83d5672482e85264
MaestroDagan <ditisdagan@hotmail.com>
2022-10-02 18:16:48 +0200

    chore: removed formatting

--- server/src/controllers/links.controller.js
+++ server/src/controllers/links.controller.js
@@ -1,30 +1,28 @@
-const { getRandomString } = require("../utils/getRandomString");
-const Link = require("../models/link");
+const {getRandomString} = require('../utils/getRandomString')
+const Link = require('../models/link')
 
 getAllLinks = async (req, res) => {
-  const allLinks = await Link.find({});
-  if (allLinks) {
-    return res.status(200).json({
-      success: true,
-      data: allLinks,
-    });
-  }
-  res.status(500).json({
-    success: false,
-    message: "Server Error - Cannot get links",
-  });
-};
+    const allLinks = await Link.find({})
+    if(allLinks){
+        return res.status(200).json({
+            "success": true,
+            "data" : allLinks
+        })
+    }
+    res.status(500).json({
+        "success": false,
+        "message": "Server Error - Cannot get links"
+    })
+}
 
 createLink = async (req, res) => {
   const minLength = 1;
   const maxLength = 6;
-  if (!req.body.url) {
-    return res
-      .status(500)
-      .json({ success: false, message: "URL is required." });
+  if(!req.body.url){
+    return res.status(500).json({"success": false, "message":"URL is required."})
   }
-  if (!/(www|http:|https:)+[^\s]+[\w]/g.test(req.body.url)) {
-    return res.status(500).json({ success: false, message: "invalid URL" });
+  if (!/(www|http:|https:)+[^\s]+[\w]/g.test(req.body.url)){
+    return res.status(500).json({ "success": false, "message": "invalid url" })
   }
   if(req.body.short.length < minLength || req.body.short.length > maxLength){ 
     return res.status(413).json({ success: false, message: "short URL isn't the right length"})
@@ -35,22 +33,22 @@ createLink = async (req, res) => {
       .status(409)
       .json({ success: false, message: "short URL already exists" });
   const shortCode = req.body.short || (await getRandomString());
-  try {
+  try{
     const link = new Link({
-      url: req.body.url,
-      short: shortCode,
-    });
-    const savedLink = await link.save();
+        url: req.body.url,
+        short: shortCode
+    })
+    const savedLink = await link.save()
     return res.status(201).json({
-      original_url: savedLink.url,
-      short_url: savedLink.short,
-    });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+        "original_url": savedLink.url,
+        "short_url": savedLink.short
+    })
+  }catch (error){
+    res.status(500).json({"success": false, "message":error.message})
   }
-};
+}
 
 module.exports = {
-  getAllLinks,
-  createLink,
-};
+    getAllLinks,
+    createLink
+}
