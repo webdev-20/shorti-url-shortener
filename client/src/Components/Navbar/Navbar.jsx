cd8d4655c5a47c62fe48c710843e6d8a593aaf37
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-10-02 16:23:01 +0400

    added react-router-dom and some navbar styling

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,35 @@
+import { NavLink } from "react-router-dom";
+
 import classes from "./Navbar.module.css";
 
 function Navbar() {
-  return <header>Navbar here</header>;
+  return (
+    <header className={classes.header}>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink to="/" className={classes.logo}>
+              Logo
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Features</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Pricing</NavLink>
+          </li>
+        </ul>
+        <ul className={classes.loginRegister}>
+          <li>
+            <NavLink to="/">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Register</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
