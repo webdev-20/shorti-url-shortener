33048345ae7219b724011f458130a8c56b5e1ab4
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-10-02 11:13:03 +0400

    added useref and improved input

--- client/src/Components/UrlShortening/UrlShortening.jsx
+++ client/src/Components/UrlShortening/UrlShortening.jsx
@@ -1,26 +1,25 @@
+import { useRef } from "react";
 import classes from "./UrlShortening.module.css";
 
 function UrlShortening() {
-  function handleInput(event) {
-    console.log(event.target.value);
-  }
+  const urlInputRef = useRef();
 
-  function handleSubmit(event) {
+  function submitHandler(event) {
     event.preventDefault();
-    console.log('submitted');
+    const enteredUrl = urlInputRef.current.value;
+    console.log(enteredUrl);
   }
 
   return (
     <div className={classes.UrlShortening}>
-      <form className={classes.form} onSubmit={handleSubmit}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <input
           id="urlInput"
           className={classes.urlInput}
-          // value=''
+          ref={urlInputRef}
           placeholder="url here"
-          // type="search"
+          type="url"
           autoComplete="off"
-          onChange={handleInput}
           autoFocus="on"
         ></input>
         <button type="submit" className={classes.shortenBtn}>
