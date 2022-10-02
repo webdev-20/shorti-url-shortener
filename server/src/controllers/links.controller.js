f151c352728428df2bcb4c22104700df4d9ff201
MaestroDagan <ditisdagan@hotmail.com>
2022-10-02 14:48:33 +0200

    feat: delete link endpoint

--- server/src/controllers/links.controller.js
+++ server/src/controllers/links.controller.js
@@ -1,44 +1,46 @@
-const {getRandomString} = require('../utils/getRandomString')
-const Link = require('../models/link')
+const { getRandomString } = require("../utils/getRandomString");
+const Link = require("../models/link");
 
 getAllLinks = async (req, res) => {
-    const allLinks = await Link.find({})
-    if(allLinks){
-        return res.status(200).json({
-            "success": true,
-            "data" : allLinks
-        })
-    }
-    res.status(500).json({
-        "success": false,
-        "message": "Server Error - Cannot get links"
-    })
-}
+  const allLinks = await Link.find({});
+  if (allLinks) {
+    return res.status(200).json({
+      success: true,
+      data: allLinks,
+    });
+  }
+  res.status(500).json({
+    success: false,
+    message: "Server Error - Cannot get links",
+  });
+};
 
 createLink = async (req, res) => {
-    if(!req.body.url){
-        return res.status(500).json({"success": false, "message":"URL is required."})
-    }
-    if (!/(www|http:|https:)+[^\s]+[\w]/g.test(req.body.url)){
-        return res.status(500).json({ "success": false, "message": "invalid url" })
-    }
-    const randomString = await getRandomString()
-    try{
-        const link = new Link({
-            url: req.body.url,
-            short: randomString
-        })
-        const savedLink = await link.save()
-        return res.status(201).json({
-            "original_url": savedLink.url,
-            "short_url": savedLink.short
-        })
-    }catch (error){
-        res.status(500).json({"success": false, "message":error.message})
-    }
-}
+  if (!req.body.url) {
+    return res
+      .status(500)
+      .json({ success: false, message: "URL is required." });
+  }
+  if (!/(www|http:|https:)+[^\s]+[\w]/g.test(req.body.url)) {
+    return res.status(500).json({ success: false, message: "invalid url" });
+  }
+  const randomString = req.body.short ?? (await getRandomString());
+  try {
+    const link = new Link({
+      url: req.body.url,
+      short: randomString,
+    });
+    const savedLink = await link.save();
+    return res.status(201).json({
+      original_url: savedLink.url,
+      short_url: savedLink.short,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 module.exports = {
-    getAllLinks,
-    createLink
-}
+  getAllLinks,
+  createLink,
+};
