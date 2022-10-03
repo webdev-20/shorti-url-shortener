95057c6e3c56335ff20810b547b201390491732e
Cheryl M <webmaster@cherylli.com>
2022-10-03 23:33:13 +1100

    minor fixes

--- server/src/controllers/links.controller.js
+++ server/src/controllers/links.controller.js
@@ -20,19 +20,33 @@ createLink = async (req, res) => {
   const minLength = 1;
   const maxLength = 6;
   if(!req.body.url){
-    return res.status(500).json({"success": false, "message":"URL is required."})
+    return res.status(500).json({
+        "success": false,
+        "message":"URL is required."
+    })
   }
   if (!/(www|http:|https:)+[^\s]+[\w]/g.test(req.body.url)){
-    return res.status(500).json({ "success": false, "message": "invalid url" })
+    return res.status(500).json({
+        "success": false,
+        "message": "invalid url"
+    })
   }
   if(req.body.short.length < minLength || req.body.short.length > maxLength){ 
-    return res.status(413).json({ success: false, message: "short URL isn't the right length"})
+    return res.status(422).json({
+        success: false,
+        message: "short URL isn't the right length"})
   }
-  if (!/(-)+[^[a-zA-Z]+$]/.test(req.body.short)) return res.status(400).json({ success: false, message: "short URL has invalid characters"})
+  if (!/(-)+[^[a-zA-Z]+$]/.test(req.body.short))
+      return res.status(422).json({
+          success: false,
+          message: "short URL has invalid characters"
+      })
   if (await Link.findOne({ short: req.body.short }))
     return res
-      .status(409)
-      .json({ success: false, message: "short URL already exists" });
+      .status(409).json({
+            success: false,
+            message: "short URL already exists"
+      });
   const shortCode = req.body.short || (await getRandomString());
   try{
     const link = new Link({
