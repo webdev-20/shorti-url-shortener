a191eba2500ad4be945393252db36461286eea1d
Gretchen Schadegg <gschadegg@gmail.com>
2022-10-07 19:54:06 -0600

    cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import UrlShortening from './Components/UrlShortening/UrlShortening';
-import './App.css';
 import ShortLinkList from './Components/ShortLinks/ShortLinksList';
+
+import './App.css';
 
 function App() {
   return (
