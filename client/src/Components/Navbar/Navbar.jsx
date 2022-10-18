7e7259342e5677f02d67532bfc640a634d2b57b9
Nancy Fontanez <100955909+nfontanez1622@users.noreply.github.com>
2022-10-18 18:38:06 -0400

    navbar styled, logo  and background added

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -6,26 +6,34 @@ function Navbar() {
   return (
     <header className={classes.header}>
       <nav className={classes.nav}>
-        <ul>
-          <li>
-            {/* temporary Logo placeholder */}
-            <NavLink to="/" className={classes.logo}>
-              Logo
-            </NavLink>
-          </li>
+        {/* <ul>
+          <li> */}
+        {/* temporary Logo placeholder */}
+        <div className={classes.logo}>
+          <img
+            src="https://raw.githubusercontent.com/webdev-20/shorti-url-shortener/96674d3d05ebd8d4f98b613bbd165f7ebe2f7056/client/public/images/shorti.svg"
+            width={247}
+            height={115}
+          ></img>
+        </div>
+        {/* </li>
           <li className={classes.featuresPricing}>
             <NavLink to="/">Features</NavLink>
           </li>
           <li className={classes.featuresPricing}>
             <NavLink to="/">Pricing</NavLink>
           </li>
-        </ul>
+        </ul> */}
         <ul className={classes.loginRegister}>
           <li>
-            <NavLink to="/">Login</NavLink>
+            <NavLink to="/" className={classes.signup}>
+              Sign Up
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/">Register</NavLink>
+            <NavLink to="/" className={classes.login}>
+              Login
+            </NavLink>
           </li>
         </ul>
       </nav>
