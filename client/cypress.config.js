4f49af592e726eaf0635553d8dc900b948bcbcf0
Cheryl M <webmaster@cherylli.com>
2023-02-24 22:30:20 +1100

    change cypress baseurl

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://127.0.0.1:5173',
     responseTimeout: 15000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
